a4aa2a9e11279a024712198dbfab90500e75ec67
mohaemdwalid <mohamedwalid680@gmail.com>
2023-07-13 16:15:30 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   puts("\"with proper grammar, but the outcome is a piece of art,");
+   
+   puts("\"Programming is like building a multilingual puzzle");
    return (0);
 }
